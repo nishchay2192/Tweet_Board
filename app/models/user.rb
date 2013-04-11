40afe68514f569cb144eb0a038e77a0039dfa39b
nishchay <nishchay@iitj.ac.in>
2013-04-11 05:19:55 -0700

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,12 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
-class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  before_save { |user| user.email = email.downcase }
-  before_save :create_remember_token
-  validates :name, presence: true, length: { maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
-end
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
-
+  before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    name     "Nishchay Kala"
-    email    "nishchay@iitj.ac.in"
-    password "123456"
-    password_confirmation "123456"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+    factory :admin do
+      admin true
+    end
   end
 end
